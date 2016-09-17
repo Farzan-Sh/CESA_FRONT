f8242c1024697babc2ce05a4a39ec5100a1de220
ammoradi <amirmohammad.moradi@gmail.com>
2016-09-17 15:27:31 +0430

    mobile menu added 10%

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(window).on('scroll', function () {
-    if ( window.innerWidth >= 768 ) {
+    if ( window.innerWidth >= 1024 ) {
         if ($(window).scrollTop() > 120) {
             $('.menu').addClass('scrolled');
             $('.header').hide();
@@ -9,4 +9,24 @@ $(window).on('scroll', function () {
                 $('.header').show();
         }
     }
+    else {
+        if ($(window).scrollTop() > 50) {
+            $('.openMenu').addClass('scrolled');
+            // $('.header').hide();
+        }
+        else {
+            $('.openMenu').removeClass('scrolled');
+            // $('.header').show();
+        }
+    }
+});
+$('.openMenu').click(function () {
+    $('.mobileMenu').animate({
+        marginRight : '0'
+    })
+});
+$('.closeMenu').click(function () {
+    $('.mobileMenu').animate({
+        marginRight : '-1000px'
+    })
 });
